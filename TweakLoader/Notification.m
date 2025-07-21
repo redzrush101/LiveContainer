4a6408a66e86d82bc3658d3f5b103e3328c26562
Huge_Black <wwkq03@outlook.com>
2025-07-21 20:30:09 +0800

    Fix sidestore's notification

---
+++ TweakLoader/Notification.m
@@ -0,0 +1,19 @@
+//
+//  Notification.m
+//  LiveContainer
+//
+//  Created by s s on 2025/7/21.
+//
+
+#import "UserNotifications/UserNotifications.h"
+#import "utils.h"
+@interface UNUserNotificationCenter(private)
+@property (nonatomic, copy) NSString *bundleIdentifier;
+@end
+
+__attribute__((constructor))
+static void UNHooksInit(void) {
+    if(NSUserDefaults.isSideStore) {
+        [UNUserNotificationCenter.currentNotificationCenter setBundleIdentifier:[NSUserDefaults.lcMainBundle bundleIdentifier]];
+    }
+}
